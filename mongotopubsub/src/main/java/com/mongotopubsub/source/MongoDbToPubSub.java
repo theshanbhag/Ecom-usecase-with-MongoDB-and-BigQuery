feeb96254855c1075bab516363dd9ecc1c54f053
achintpeer <111537542+achintpeer@users.noreply.github.com>
2022-08-22 16:22:25 -0500

    Updating mongotopubsub code after changing to read certain variables from environment variables

--- mongotopubsub/src/main/java/com/mongotopubsub/source/MongoDbToPubSub.java
+++ mongotopubsub/src/main/java/com/mongotopubsub/source/MongoDbToPubSub.java
@@ -14,26 +14,27 @@ import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.model.changestream.ChangeStreamDocument;
 import com.mongodb.client.model.changestream.FullDocument;
 import org.bson.Document;
-import org.bson.types.Decimal128;
-
-import java.math.BigDecimal;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 
 public class MongoDbToPubSub {
 
+    private static final Logger LOG = LoggerFactory.getLogger(MongoDbToPubSub.class);
 
     public static void main(String[] args) throws Exception {
-        String PROJECT_ID = "ecomm-analysis";
-        String topicId = "ecommerce_topic";
 
+        String PROJECT_ID = System.getenv("ProjectId");
 
-        MongoClient mongoClient = new MongoClient(new MongoClientURI("mongodb+srv://mongotobq:M0ngoToBqPoc@mongo-bq.kn30v.mongodb.net"));
-        MongoDatabase mongoDatabase = mongoClient.getDatabase("ecommerce");
-        MongoCollection mongoCollection =  mongoDatabase.getCollection("orders");
+        String topicId = System.getenv("TopicId");
+
+        MongoClient mongoClient = new MongoClient(new MongoClientURI(System.getenv("MongoURI")));
+
+        MongoDatabase mongoDatabase = mongoClient.getDatabase(System.getenv("Database"));
+
+        MongoCollection mongoCollection =  mongoDatabase.getCollection(System.getenv("Collection"));
 
         ProjectTopicName topicName = ProjectTopicName.of(PROJECT_ID, topicId);
         List<ApiFuture<String>> futures = new ArrayList<>();
@@ -42,22 +43,15 @@ public class MongoDbToPubSub {
         Block<ChangeStreamDocument<Document>> publishToPubSub = new Block<ChangeStreamDocument<Document>>() {
             @Override
             public void apply(final ChangeStreamDocument<Document> changeStreamDocument) {
-                //System.out.println(changeStreamDocument.getFullDocument().toJson());
+
 
                 try {
-                    System.out.println(changeStreamDocument.getFullDocument());
+
                     Document fullDocument = (Document)changeStreamDocument.getFullDocument();
                     String id = fullDocument.get("_id").toString();
                     fullDocument.put("_id",id);
-                   /* String saleDate = fullDocument.get("saleDate").toString();
-                    fullDocument.put("saleDate",saleDate);*/
-                    //fullDocument.remove("items");
-                   /* List<Document> itemsDoc = (ArrayList)fullDocument.get("items");
-                    itemsDoc.stream().forEach(item -> {
-                        item.put("price",Float.parseFloat(item.get("price").toString()));
-                    });*/
-                    //fullDocument.remove("customer");
-                    System.out.println("Document is:"+fullDocument.toJson());
+
+                    LOG.info("Document is:"+fullDocument.toJson());
 
                     ByteString data = ByteString.copyFromUtf8(fullDocument.toJson());
                     PubsubMessage pubsubMessage = PubsubMessage.newBuilder()
